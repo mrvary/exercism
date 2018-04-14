07a01e97d6943b806e707088df1ff20756c40da8
mrvary <felix.breitenbach@gmail.com>
2018-04-15 00:21:26 +0200

    very dense solution for this problem (pretty much copied from someone else)

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,23 +1,11 @@
 (ns armstrong-numbers)
 
-(defn exp [x n]
+(defn ** [x n]
   (reduce * (repeat n x)))
 
-(defn digit-sum [num exponent]
-  ;; sums digits each raised to the power of exp
-  (loop [current-num num
-         sum 0]
-    (if (= 0 current-num)
-      sum
-      (recur (quot current-num 10)
-             (+ sum (exp (mod current-num 10) exponent))))))
-
-(defn num-of-digits [n]
-  (if (< n 10)
-      1
-      (inc (num-of-digits (quot n 10)))))
-
 (defn armstrong? [n]
-  (= (digit-sum n (num-of-digits n)) n))
-
+  (let [digits (count (str n))
+        result (reduce + (map #(** (Integer/parseInt (str %)) digits) (str n)))]
+    (= n result)))
+       
 
