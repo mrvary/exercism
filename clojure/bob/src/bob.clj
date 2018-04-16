cac0aa5b162730a2f515393c3f0af7535267e9c0
mrvary <felix.breitenbach@gmail.com>
2018-04-16 11:37:17 +0200

    bob solved

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,5 +1,20 @@
-(ns bob)
+(ns bob
+  (:use [clojure.string :only [blank? upper-case]]))
 
-(defn response-for [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn question?
+  [x]
+  (.endsWith x "?"))
+
+(defn yelling?
+  [x]
+  (and (re-find #"[A-Z]" x)
+       (= (upper-case x) x)))
+
+(defn response-for
+  ([] "Fine. Be that way!")
+  ([input]
+   (cond
+     (blank? input) (response-for)
+     (yelling? input) "Whoa, chill out!"
+     (question? input) "Sure."
+     :else "Whatever.")))
