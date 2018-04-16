6e51ef6507c988f623f7d745c6a419561087cee4
mrvary <felix.breitenbach@gmail.com>
2018-04-16 12:02:39 +0200

    rna-transcription finished

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,5 +1,14 @@
 (ns rna-transcription)
 
-(defn to-rna [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn rna-complement
+  [x]
+  (cond
+    (= x \G) "C"
+    (= x \C) "G"
+    (= x \A) "U"
+    (= x \T) "A"
+    :else (throw (AssertionError. "Wrong input."))))
+
+(defn to-rna
+  [input]
+  (apply str (map rna-complement input)))
