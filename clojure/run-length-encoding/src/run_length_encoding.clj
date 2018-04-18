87a728add5a08a3db863a63e445d9a6af94f2a05
mrvary <felix.breitenbach@gmail.com>
2018-04-18 11:26:27 +0200

    removed .gitignore - probably not needed that much

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -1,11 +1,29 @@
 (ns run-length-encoding)
 
+(defn repeat-char
+  [pair]
+  (let [[n c] pair]
+    (apply str (repeat (Integer/parseInt (str n)) (str c)))))
+
+(defn num?
+  [x]
+  (re-matches #"\d" (str x)))
+
+(defn parse
+  [s]
+  (loop [result []
+         coll s]))
+
 (defn run-length-encode
   "encodes a string with run-length-encoding"
-  [s]
-  )
+  [s])
+
+
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
   [s]
-  )
+  (cond
+    (empty? s) ""
+    (num? (first s)) (str (repeat-char (take 2 s)) (run-length-decode (nnext s)))
+    :else (str (first s) (run-length-decode (next s)))))
