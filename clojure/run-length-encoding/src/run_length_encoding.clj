63b21c6ae7e0c32f949bf4bf1f84a425919ebff8
mrvary <felix.breitenbach@gmail.com>
2018-04-18 15:08:55 +0200

    run-length-encoding finally done

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -1,22 +1,13 @@
 (ns run-length-encoding)
 
-(defn- repeat-char
-  [pair]
-  (let [[n c] pair]
-    (apply str (repeat (Integer/parseInt (str n)) (str c)))))
-
-(defn num?
-  [x]
-  (re-matches #"\d" (str x)))
+;; ENCODE
 
 (defn- reduce-chars
-  "takes a seq of chars and returns a list of (length value)"
+  "takes a seq of chars and returns '(length value) for duplicates"
   [chars]
   (let [length (count chars)
         char (first chars)]
-    (if (> length 1)
-      (list length char)
-      char)))
+    (if (> length 1) (list length char) char)))
 
 (defn run-length-encode
   "encodes a string with run-length-encoding"
@@ -27,10 +18,38 @@
        (flatten)
        (clojure.string/join)))
 
+;; DECODE
+
+(defn- repeat-char
+  "repeats char c n times, the output is a seq of single digit strings"
+  [[n c]]
+  (repeat (Integer/parseInt (str n)) (str c)))
+
+(defn- num?
+  "returns true if input char is a number"
+  [x]
+  (re-matches #"\d+" (str x)))
+
+(defn- split-input
+  "splits '3a35bsd' into '(\"3\" \"a\" \"35\" \"b\" \"s\" \"d\") "
+  [s]
+  (re-seq #"[a-zA-Z ]|\d+" s))
+
+(defn- transform-input
+  "transforms a list of numbers and chars into a list of only chars"
+  [coll]
+  (let [item (first coll)]
+    (cond
+      (empty? coll) '()
+      (num? item) (conj (transform-input (nnext coll))
+                        (repeat-char (take 2 coll)))
+      :else (conj (transform-input (next coll)) item))))
+
 (defn run-length-decode
   "decodes a run-length-encoded string"
   [s]
-  (cond
-    (empty? s) ""
-    (num? (first s)) (str (repeat-char (take 2 s)) (run-length-decode (nnext s)))
-    :else (str (first s) (run-length-decode (next s)))))
+  (->> s
+       (split-input)
+       (transform-input)
+       (flatten)
+       (clojure.string/join)))
