1b5ffd0598533d6d2af00093937eb498dd505299
mrvary <felix.breitenbach@gmail.com>
2018-04-18 12:28:21 +0200

    implemented the encode part of run-length-encoding

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -1,6 +1,6 @@
 (ns run-length-encoding)
 
-(defn repeat-char
+(defn- repeat-char
   [pair]
   (let [[n c] pair]
     (apply str (repeat (Integer/parseInt (str n)) (str c)))))
@@ -9,16 +9,23 @@
   [x]
   (re-matches #"\d" (str x)))
 
-(defn parse
-  [s]
-  (loop [result []
-         coll s]))
+(defn- reduce-chars
+  "takes a seq of chars and returns a list of (length value)"
+  [chars]
+  (let [length (count chars)
+        char (first chars)]
+    (if (> length 1)
+      (list length char)
+      char)))
 
 (defn run-length-encode
   "encodes a string with run-length-encoding"
-  [s])
-
-
+  [s]
+  (->> s
+       (partition-by identity)
+       (map reduce-chars)
+       (flatten)
+       (clojure.string/join)))
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
